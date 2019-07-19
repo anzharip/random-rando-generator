e5e3097e04b4a056d13b2f2e8f7e1a1e30a7dffd
anzhari.p@gmail.com <anzhari.p@gmail.com>
2019-07-19 15:05:13 +0700

    .

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import random
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/rando')
+def get():
+    rando = [
+        "https://i.ibb.co/7yrW3Yg/1-D586-F4-A-DBDF-4221-99-E5-35-B6-C5-FDF0-EF.jpg",
+        "https://i.ibb.co/JvmnmXW/60661557-5610-4335-A865-A6-E1-CAE4716-E.jpg",
+        "https://i.ibb.co/52D6LZ7/69851-B74-6635-42-CA-A730-AD4688-B3-A139.jpg",
+        "https://i.ibb.co/Wv3BMRk/59509-F79-CE80-439-F-BFD0-80-D5-BFA59-A8-D.jpg",
+        "https://i.ibb.co/6vSk9Lp/B9-C1-C218-3-B0-D-4-AB5-AB3-C-99-E0-A704-A810.jpg",
+        "https://i.ibb.co/NLBrGhY/3-ED2-BCF9-3738-49-B0-9-D92-CB0-B4-A36-F377.jpg",
+        "https://i.ibb.co/GtSn42X/6-F7-DD1-C0-4-C7-F-45-AF-B7-D4-269-E45-E63614.jpg",
+        "https://i.ibb.co/DWzBmHQ/8-CF656-A2-E377-48-B5-B7-C0-2-E414-A40-CABB.jpg",
+        "https://i.ibb.co/v33jC96/F8871-F27-3-A5-A-4-D55-9-E85-E4-CAC415492-B.jpg",
+    ]
+    random_rando = random.choice(rando)
+    return redirect(random_rando, code=302)
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 15000))
+    app.run(host='0.0.0.0', port=port)
---
+++ test.py
@@ -0,0 +1,19 @@
+import random
+
+rando = [
+    "https://ibb.co/12RqCvs",
+    "https://ibb.co/8rNMNFn",
+    "https://ibb.co/pnDxdqm",
+    "https://ibb.co/JjdrGSv",
+    "https://ibb.co/CJyfCDX",
+    "https://ibb.co/64C8qhs",
+    "https://ibb.co/SdC54n2",
+    "https://ibb.co/1Q7CBtT",
+    "https://ibb.co/7nn1xhd",
+]
+
+random_rando = random.choice(rando)
+
+print(
+    random_rando
+)
